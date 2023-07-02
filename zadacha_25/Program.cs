e1f1ddee1069720ceef60293f7d9d551216b21fc
Алексей Парасненко <aleksejparasnenko@MacBook-Pro-Aleksej.local>
2023-07-02 11:56:58 +0700

    zadacha 25 made

---
+++ zadacha_25/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+ 3, 5 -> 243 (3⁵) 
+ 2, 4 -> 16
+ */
+
+ int vozvedenie_priglashenie (string msg)
+ {
+    Console.Write(msg);
+    string messedg = Console.ReadLine();
+    int result = int.Parse(messedg);    
+    return result;
+
+ }
+
+int vozvedenieMathPow (int numberA, int numberB)
+{
+    int vozvedenieMathPow = 1;
+    for (int count = 0; count < numberB; count++)
+    {
+        vozvedenieMathPow *= numberA; 
+    }
+
+    return vozvedenieMathPow;
+}
+
+bool numberBproverka (int numberB)
+{
+    if (numberB < 0)
+    {
+        Console.WriteLine("our number dont <= 0");
+        return false;
+    }
+    return true;
+}
+
+int numberA = vozvedenie_priglashenie("enter number A: ");
+int numberB = vozvedenie_priglashenie("enter number B: ");
+if (numberBproverka(numberB))
+{
+    Console.WriteLine($" number {numberA} v stepene {numberB} = {vozvedenieMathPow(numberA, numberB)}");
+}
+
+ 
